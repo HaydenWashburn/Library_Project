be9c436a926636a0dd2044380e229ac0be8ef272
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-21 17:02:41 -0500

    completed the steps for exercise but have an error. Need to debug and refactor

--- app.js
+++ app.js
@@ -1,4 +1,63 @@
-console.log("Hello World!\n==========\n");
-
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+const books = [
+  {
+    title: "Name of the Wind",
+    author: "Patrick Rothfuss",
+    read: true,
+  },
+];
+
+class Book {
+  constructor(title, author, read) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+class Library {
+  constructor(books) {
+    this.bookCount = books.length;
+    this.books = books;
+  }
+
+  addBook() {
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const read = document.getElementById("read");
+    const newBook = new Book(title.value, author.value, read.value);
+    this.books.push(newBook);
+    this.bookCount++;
+    const tbody = document.getElementById("tableBody");
+    const newTr = document.createElement("tr");
+    const newTitle = document.createElement("td");
+    const newAuthor = document.createElement("td");
+    const newRead = document.createElement("td");
+
+    newTr.append(newTitle, newAuthor, newRead);
+
+    tbody.appendChild(newTr);
+  }
+
+  markRead(checkbox, id) {
+    this.books.forEach(book => {
+      if (id === book.id) {
+        book.read = true;
+        checkbox.checked = true;
+        checkbox.disabled = true;
+      }
+    });
+  }
+}
+
+const library = new Library(books);
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  library.addBook();
+});
+
+console.log(addBook());
